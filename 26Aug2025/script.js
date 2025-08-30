1a0e579b2ed2ef7799fc55ac791e1b0813acf0ed
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-08-30 16:20:15 +0200

    Update script.js

--- 26Aug2025/script.js
+++ 26Aug2025/script.js
@@ -7,20 +7,42 @@ If parentheses are nested, the innermost pair should be reversed first, and then
 Assume all parentheses are evenly balanced and correctly nested.
 */
 
-/* not my own clue, found it on 'https://stackoverflow.com/questions/42254365/reverseparentheses-codefights'
-*/
+function indices(source, find) {
+if (!source) {return [];}    let result = [];
+for (let i = 0; i < source.length; i++) {
+if (source.substring(i, i + find.length) == find) {
+result.push(i);
+  }
+ }
+return result;
+}
 
-function reverseOnce(s){
-      var regexp = /\(([^()]*)\)/i;
-          var subStr = regexp.exec(s)[1];
-              subStr = subStr.split('').reverse().join('');
-                  return s.replace(regexp, subStr)
-                  }
+function hlp(s){
+  let ind1=indices(s,"(");
+  let ind2=indices(s,")");
+  let ind=[...ind1,...ind2].sort((a,b)=>a-b);
+  let d=[];
+  for(let i=0;i<ind.length;i++){ 
+   if(ind1.includes(ind[i])&&ind2.includes(ind[i+1])){
+   d.push([ind[i],ind[i+1]]);
+   }
+  }
+  let ob={};
+for(let i=0;i<d.length;i++){
+  ob[i]=s.substring(d[i][0]+1,d[i][1]).split("").reverse().join("");
+}
+ 
+ let m=s.substring(0,d[0][0])+ob[0];
+ for(let i=0;i<d.length-1;i++){
+   m+=s.substring(d[i][1]+1,d[i+1][0])+ob[i+1];
+ }
+ m+=s.substring(d[d.length-1][1]+1);
+  return m;
+}
 
 function decode(s) {
 if (s.includes('(')){
-        return decode(reverseOnce(s));
-            } else {     
-    return s;
-    }
-}
+  console.log(hlp(s));
+  return decode(hlp(s));
+  } else {return s;}
+} 
