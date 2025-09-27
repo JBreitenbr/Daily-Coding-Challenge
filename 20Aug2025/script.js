b0bc58b5c3e9b6d71fec00f387c97e6904558e43
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-09-27 19:37:11 +0200

    Delete 20Aug2025 directory

--- 20Aug2025/script.js
+++
@@ -1,9 +0,0 @@
-/* 3 Strikes
-Given an integer between 1 and 10,000, return a count of how many numbers from 1 up to that integer whose square contains at least one digit 3.
-*/
-
-function squaresWithThree(n) {
-  let r=Array.from(new Array(n).keys()).slice(1);
-  let m=r.map((item)=>(item*item).toString()).filter((item)=>item.includes('3'));
-  return m.length;
-}
