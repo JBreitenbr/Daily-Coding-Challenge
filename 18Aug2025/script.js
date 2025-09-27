d16cd8d1db61250cb7da93a0736a97ad0f307641
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-09-27 19:38:18 +0200

    Delete 18Aug2025 directory

--- 18Aug2025/script.js
+++
@@ -1,9 +0,0 @@
-/* Factorializer
-Given an integer from zero to 20, return the factorial of that number. The factorial of a number is the product of all the numbers between 1 and the given number.
-
-The factorial of zero is 1. */
-
-function factorial(n) {
-  if(n==0) return 1;
-  else return n*factorial(n-1);
-}
